d42c6886ecfe0485dd659a9744bcaeb1d839d53b
Kariz Alberto <kariz.alberto@yahoo.com>
2018-07-04 18:44:33 -0700

    Create a new Player object

--- js/app.js
+++ js/app.js
@@ -40,8 +40,7 @@ Player.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+var player = new Player(202, 405);
 
 
 // This listens for key presses and sends the keys to your
